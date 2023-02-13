4ccbc8086e7544721b137a909a6fb95be2a742d1
Rus7Iv <rusivary@gmail.com>
2023-02-13 16:19:57 +0400

    Добавлены блоки операторов связи

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,13 +28,117 @@ export default function Home() {
               scale: 1,
               opacity: 1,
               transition: {
-                delay: 0.4,
+                delay: 0.2,
               },
             },
           }}
         >
           <h1 className="title">Web-terminal</h1>
         </motion.div>
+        <div className="operators-card">
+          <motion.li
+            className="item"
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.4,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.2,
+                },
+              },
+            }}
+            whileHover={{
+              position: "relative",
+              zIndex: 1,
+              background: "white",
+              scale: 1.2,
+              transition: {
+                duration: 0.2,
+              },
+            }}
+          >
+            <a href="">
+              <img
+                src="https://toplogos.ru/images/thumbs/preview-logo-mts.png"
+                alt="Логотип MTS"
+              />
+            </a>
+          </motion.li>
+          <motion.li
+            className="item"
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.5,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.2,
+                },
+              },
+            }}
+            whileHover={{
+              position: "relative",
+              zIndex: 1,
+              background: "white",
+              scale: 1.2,
+              transition: {
+                duration: 0.2,
+              },
+            }}
+          >
+            <a href="">
+              <img
+                src="https://toplogos.ru/images/thumbs/preview-logo-megafon.png"
+                alt="Логотип Megafon"
+              />
+            </a>
+          </motion.li>
+          <motion.li
+            className="item"
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.4,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.2,
+                },
+              },
+            }}
+            whileHover={{
+              position: "relative",
+              zIndex: 1,
+              background: "white",
+              scale: 1.2,
+              transition: {
+                duration: 0.2,
+              },
+            }}
+          >
+            <a href="">
+              <img
+                src="https://toplogos.ru/images/thumbs/preview-logo-beeline.png"
+                alt="Логотип Beeline"
+              />
+            </a>
+          </motion.li>
+        </div>
       </main>
     </>
   );
